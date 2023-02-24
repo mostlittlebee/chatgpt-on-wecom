e9cc37986605c0941d9642fe0504a0acf5d814af
大米 <66151739+mostlittlebee@users.noreply.github.com>
2023-02-24 22:39:53 +0800

    修复WechatEnterpriseChannel类名

--- channel/wechatcom/wechatenterprise_channel.py
+++ channel/wechatcom/wechatenterprise_channel.py
@@ -20,24 +20,28 @@ from flask import Flask, request ,abort
 thread_pool = ThreadPoolExecutor(max_workers=8)
 app = Flask(__name__)
 
-class WechatChannel(Channel):
+@app.route('/wechat', methods=['GET','POST'])
+def handler_msg():
+    WechatEnterpriseChannel().handle()
+    return 'success'
+
+class WechatEnterpriseChannel(Channel):
     def __init__(self):
-        pass
+        self.CorpId = conf().get('WECHAT_CORP_ID')
+        self.Secret = conf().get('Secret')
+        self.AppId = conf().get('AppId')
+        self.TOKEN = conf().get('WECHAT_TOKEN')
+        self.EncodingAESKey = conf().get('WECHAT_ENCODING_AES_KEY')
+        self.crypto = WeChatCrypto(self.TOKEN, self.EncodingAESKey, self.CorpId)
+        self.client = WeChatClient(self.CorpId, self.Secret,self.AppId)
 
     def startup(self):
         # start message listener
         app.run(host='0.0.0.0',port=8888)
-      
-        
-        
 
     def send(self, msg, receiver):
         logger.info('[WXCOM] sendMsg={}, receiver={}'.format(msg, receiver))
-        CorpId = conf().get('WECHAT_CORP_ID')
-        Secret = conf().get('Secret')
-        AppId = conf().get('AppId')
-        client = WeChatClient(CorpId, Secret,AppId)
-        client.message.send_text(AppId,receiver,msg)
+        self.client.message.send_text(self.AppId,receiver,msg)
 
     def _do_send(self, query, reply_user_id):
         try:
@@ -51,31 +55,23 @@ class WechatChannel(Channel):
         except Exception as e:
             logger.exception(e)
 
-    @app.route('/wechat', methods=['GET','POST'])
-    def handle(self, msg):
+    def handle(self):
         query_params = request.args
         signature = query_params.get('msg_signature', '')
         timestamp = query_params.get('timestamp', '')
         nonce = query_params.get('nonce', '')    
-        TOKEN = conf().get('WECHAT_TOKEN')
-        EncodingAESKey = conf().get('WECHAT_ENCODING_AES_KEY')
-        CorpId = conf().get('WECHAT_CORP_ID')
-        crypto = WeChatCrypto(TOKEN, EncodingAESKey, CorpId)
-        Secret = conf().get('Secret')
-        AppId = conf().get('AppId')
-        client = WeChatClient(CorpId, Secret,AppId)
         if request.method == 'GET':
             # 处理验证请求
             echostr = query_params.get('echostr', '')
             try:
-                echostr = crypto.check_signature(signature, timestamp, nonce, echostr)
+                echostr = self.crypto.check_signature(signature, timestamp, nonce, echostr)
             except InvalidSignatureException:
                 abort(403)
             print(echostr)
             return echostr
         elif request.method == 'POST':
             try:
-                message = crypto.decrypt_message(
+                message = self.crypto.decrypt_message(
                     request.data,
                     signature,
                     timestamp,
@@ -86,8 +82,8 @@ class WechatChannel(Channel):
             msg = parse_message(message)
             if msg.type == 'text':
                 reply = '收到，思考中...'
-                thread_pool.submit(_do_send, msg.content, msg.source)
+                thread_pool.submit(self._do_send, msg.content, msg.source)
             else:
                 reply = 'Can not handle this for now'
-            client.message.send_text(AppId,msg.source,reply)
+            self.client.message.send_text(self.AppId,msg.source,reply)
             return 'success'
